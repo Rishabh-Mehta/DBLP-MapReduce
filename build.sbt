527da3e8446bf1113827f287edb6526a64ad6676
rishabh <rmehta35@uic.edu>
2020-10-17 15:12:32 -0500

    assembly

--- build.sbt
+++ build.sbt
@@ -3,3 +3,27 @@ name := "rishabh_mehta_hw2"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+// https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-common
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "3.3.0"
+
+// https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-mapreduce-client-core
+libraryDependencies += "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "3.3.0"
+
+
+// https://mvnrepository.com/artifact/com.typesafe/config
+libraryDependencies += "com.typesafe" % "config" % "1.2.1"
+
+// https://mvnrepository.com/artifact/com.typesafe.scala-logging/scala-logging
+//libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+
+// https://mvnrepository.com/artifact/org.scala-lang.modules/scala-xml
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M2"
+
+// https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
+libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.2" % Test
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs@_*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/com/dblp/mapreduce/MapReduce.scala
@@ -0,0 +1,77 @@
+package com.dblp.mapreduce
+
+import com.dblp.mapreduce.XMLInput.DBLPXmlInputFormat
+import com.dblp.mapreduce.utils.ApplicationConstants
+import com.typesafe.config.ConfigFactory
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, TextOutputFormat}
+import org.apache.log4j.BasicConfigurator
+import org.slf4j.LoggerFactory
+
+object MapReduce {
+  BasicConfigurator.configure()
+  val logger = LoggerFactory.getLogger(MapReduce.getClass)
+
+
+  def main(args: Array[String]): Unit = {
+
+    logger.info("Starting Map Reduce Jobs")
+    val configObj = ConfigFactory.load().getConfig(ApplicationConstants.MAP_REDUCE_CONFIG)
+    val inputpath = ConfigFactory.load().getString(ApplicationConstants.INPUT_PATH)
+    val jobs = ConfigFactory.load().getString(ApplicationConstants.JOBS).split(",").toList
+
+    jobs.foreach(job => {
+      if (job.trim.equals("AuthorCount")) {
+        logger.info("Starting Co-Author Count Job")
+        //AuthorCount.runJob(configObj.inputFile, configObj.outputFile)
+        val configuration = new Configuration
+        val job1 = Job.getInstance(configuration, job)
+        val inputPath = new Path("home/hadoop/input")
+        val outputPathFirstJob = new Path("home/hadoop/output")
+        outputPathFirstJob.getFileSystem(configuration).delete(outputPathFirstJob, true)
+        logger.info("Here")
+        job1.setJarByClass(this.getClass)
+        job1.setMapperClass(classOf[com.dblp.mapreduce.author_venue.Author_Venue.Map])
+        job1.setReducerClass(classOf[com.dblp.mapreduce.author_venue.Author_Venue.Reduce])
+        job1.setInputFormatClass(classOf[DBLPXmlInputFormat])
+        job1.setOutputFormatClass(classOf[TextOutputFormat[_, _]])
+        job1.setMapOutputKeyClass(classOf[Text])
+        job1.setMapOutputValueClass(classOf[IntWritable])
+        job1.setOutputKeyClass(classOf[Text])
+        job1.setOutputValueClass(classOf[IntWritable])
+        FileInputFormat.addInputPath(job1, inputPath)
+        FileOutputFormat.setOutputPath(job1, outputPathFirstJob)
+        System.exit(if (job1.waitForCompletion(true)) 0
+        else 1)
+
+
+      } else if (job.trim.equals("PublicationYearStratification")) {
+        logger.info("Starting Co-Author count, Stratification by Publication Year job")
+        //PublicationYearStratification.runJob(configObj.inputFile, configObj.outputFile)
+      } else if (job.trim.equals("PublicationVenueStratification")) {
+        logger.info("Starting Co-Author count, Stratification by Publication Venue job")
+        //PublicationVenueStratification.runJob(configObj.inputFile, configObj.outputFile)
+      } else if (job.trim.equals("Authorship")) {
+        logger.info("Starting Authorship and Top 100 and least 100 collaborating authors job")
+        //Authorship.runJob(configObj.inputFile, configObj.outputFile)
+      }
+      else if (job.trim.equals("MeanMedianStatistics")) {
+        logger.info("Starting MeanMedianStatistics calculation job")
+        //MeanMedianStatistics.runJob(configObj.inputFile, configObj.outputFile)
+      }
+      else if (job.trim.equals("MeanMedianStatisticsWithStratification")) {
+        logger.info("Starting MeanMedianStatisticsWithStratification calculation job")
+        //MeanMedianStatisticsWithStratification.runJob(configObj.inputFile, configObj.outputFile)
+      }
+    })
+    logger.info("Completed All Jobs Successfully")
+  }
+}
+
+
+
+
